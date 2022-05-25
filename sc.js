fc7a2bc21d19d3a366c90d55e6003e7dabdca2cd
Abolfazl-MI <abolfazlmashhadi837@gmail.com>
2022-05-25 20:52:29 +0430

    Trigger rebuild

---
+++ sc.js
@@ -0,0 +1,14 @@
+// const sections = document.querySelectorAll("section");
+// const navLi = document.querySelectorAll(".navbar .container ul li");
+// window.addEventListener("scroll", () => {
+//   let  = "";
+//   sections.forEach((section) => {
+//     const sectionTop = section.offsetTop;
+//     const sectionHeight = section.clientHeight;
+//     if (pageYOffset >= sectionTop - sectionHeight / 3) {
+//       current = section.getAttribute("id");
+//     }
+//     console.log(current);
+//   });
+// });
+
